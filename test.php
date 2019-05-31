1f8aa660c5c20b3fe6c8ef2acc2517576d4444b9
Alessandro Spisso <sps_97@hotmail.it>
2019-05-31 12:13:32 +0200

    style + test php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+$db_host = "localhost";
+$db_user = "pweb1819massaros";
+$db_name = "my_pweb1819massaros";
+
+$db = mysql_connect($db_host, $db_user);
+if ($db == FALSE)
+die ("Errore nella connessione. Verificare i parametri nel file config.php");
+$ris = mysql_select_db($db_name, $db);
+if ($ris == false) die ("Errore nella selezione del database. Verificare i parametri nel file config.php"); 
+
+$sql = "SELECT * FROM punti_incontro WHERE id = 2";
+$result = $db->query($sql);
+
+
+?>
