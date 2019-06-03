bf1aa07e62014ad3bdeef6a42e76ce6e9ce7f64a
Simone Massaro <simonemassaro7@gmail.com>
2019-06-03 16:57:41 +0200

    Connection Database

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "pweb1819massaros";
+    $db_name = "my_pweb1819massaros";
+
+    $db = mysql_connect($db_host, $db_user);
+    if ($db == FALSE)
+    die ("Errore nella connessione. Verificare i parametri nel file config.php");
+    $ris = mysql_select_db($db_name, $db);
+    if ($ris == false) die ("Errore nella selezione del database. Verificare i parametri nel file config.php"); 
+?>
--- test.php
+++ test.php
@@ -1,17 +1,9 @@
 <?php
-$db_host = "localhost";
-$db_user = "pweb1819massaros";
-$db_name = "my_pweb1819massaros";
+    @include 'config.php';
 
-$db = mysql_connect($db_host, $db_user);
-if ($db == FALSE)
-die ("Errore nella connessione. Verificare i parametri nel file config.php");
-$ris = mysql_select_db($db_name, $db);
-if ($ris == false) die ("Errore nella selezione del database. Verificare i parametri nel file config.php"); 
+    $sql = "SELECT * FROM punti_incontro WHERE id = 2";
+    $result = mysql_query($sql);
 
-$sql = "SELECT * FROM punti_incontro WHERE id = 2";
-$result = mysql_query($sql);
-
-$lol = mysql_fetch_array($result);
-echo $lol[nome];
+    $lol = mysql_fetch_array($result);
+    echo $lol[nome];
 ?>
