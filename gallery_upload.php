481cf3138a4207f2c17bd5cf69c987c9531ec94a
Simone Massaro <simonemassaro7@gmail.com>
2019-06-13 18:26:27 +0200

    Update gallery_upload.php

--- gallery_upload.php
+++ gallery_upload.php
@@ -15,21 +15,22 @@
             <div id="navbar">
                 <ul>
                     <li><a href="index.html">Home</a></li>
-                    <li><a href="recognize.html">Sintomi</a></li>
-                    <li><a href="stop.html">Smettere</a></li>
+                    <li><a href="recognize.html">Info</a></li>
+                    <!-- <li><a href="stop.html">Smettere</a></li> -->
                     <li><a class="active" href="gallery.php">Gallery</a></li>
-                    <li><a href="map.html">Mappa</a></li>
-                    <li><a href="contactus.html">Contattaci</a></li>
+                    <li><a href="map.php">Mappa</a></li>
+                    <li><a href="contactus.php">Contattaci</a></li>
                 </ul>
             </div>
         </header>
         <div id="container">
+            <div style="text-align:center">
+                <h2>Carica le tue Immagini</h2>
+            </div>
             <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>
                 " enctype="multipart/form-data">
                 Titolo:<br />
                 <input name="titolo" type="text" size="20"><br />
-                Descrizione:<br />
-                <textarea name="descrizione" cols="20" rows="4"></textarea><br />
                 Immagine:<br />
                 <input type="file" name="imagefile"><br />
                 <input type="submit" name="Submit" value="Submit">
@@ -38,50 +39,67 @@
                     if(isset($_POST['Submit']))
                     {
                         @include 'config.php';
-                        @require 'gallery_function.php';
+                        // Cartella fisica in cui andremo ad inserire le immagini
+                        $path_img = 'gallery/';
 
-                        // Creo una array con i formati accettati
+                        // Verificare che l'upload non sovrascriva altro file
+                        $target_file = $path_img . $_FILES['imagefile']['name'];
+                        if (file_exists($target_file)) 
+                        {
+                            echo 'Il file esiste già';
+                            exit;
+                        }
+                        // Verificare l'estensione del file caricato
                         $tipi_consentiti = array("image/gif","image/jpeg","image/png");
+                        if (!@in_array($_FILES['imagefile']['type'], $tipi_consentiti))
+                        {
+                            echo 'Il file ha un estensione non ammessa!';
+                            exit;
+                        }
 
-                        // verifico che il formato del file sia tra quelli accettati
-                        if (@in_array($_FILES['imagefile']['type'], $tipi_consentiti))
-                        { 
-                            // copio il file nella cartella delle immagini
-                            @copy ($_FILES['imagefile']['tmp_name'], $path_img . $_FILES['imagefile']['name']);
-
-                            // recupero i dati dal form
-                            $titolo = @addslashes($_POST['titolo']);
-                            $descrizione = @addslashes($_POST['descrizione']);
-                            $nome = @addslashes($_FILES['imagefile']['name']);
-                            $path = $path_img . stripslashes($nome);
-                            $tipo = @addslashes($_FILES['imagefile']['type']);
-
-                            // creo la miniatura
-                            @makeThumb($path_img,$path,$nome,$tipo);
+                        // copio il file nella cartella delle immagini
+                        @copy ($_FILES['imagefile']['tmp_name'], $path_img . $_FILES['imagefile']['name']);
+                        // recupero i dati dal form
+                        $titolo = @addslashes($_POST['titolo']);
+                        $nome = @addslashes($_FILES['imagefile']['name']);
+                        $path = $path_img . stripslashes($nome);
+                        $tipo = @addslashes($_FILES['imagefile']['type']);
+                        // aggiorno il database
+                        $query = "INSERT INTO images (Titolo,Nome,Tipo) VALUES('$titolo','$nome','$tipo')";
+                        $res = @mysql_query($query) or die (mysql_error());
+                        @mysql_close($cn);
                         
-                            // aggiorno il database
-                            $query = "INSERT INTO images (Titolo,Descrizione,Nome,Tipo) VALUES('$titolo','$descrizione','$nome','$tipo')";
-                            $res = @mysql_query($query) or die (mysql_error());
-                            @mysql_close($cn);
-
-                            // Stampo a video un po' di informazioni
-                            echo "Nome: ".$_FILES['imagefile']['name']."<br />"; 
-                            echo "Dimensione: ".$_FILES['imagefile']['size']."<br />"; 
-                            echo "Tipo: ".$_FILES['imagefile']['type']."<br />"; 
-                            echo "Copia eseguita con successo."; 
+                        //Verificare se il file è stato caricato
+                        if (!isset($_FILES['imagefile']) || !is_uploaded_file($_FILES['imagefile']['tmp_name'])) 
+                        {
+                            echo "Impossibile eseguire l'upload.";
+                            exit;    
                         }
                         else
                         {
-                            // stampo un messaggio di errore nel caso in cui il file sia di un formato non consentito
-                            echo "Impossibile eseguire l'upload.";
+                            // Stampo a video un po' di informazioni
+                            echo "<div style=\"display: none; margin-top: 15px;\">"
+                                echo "Nome: ".$_FILES['imagefile']['name']."<br />"; 
+                            echo "Nome: ".$_FILES['imagefile']['name']."<br />"; 
+                                echo "Nome: ".$_FILES['imagefile']['name']."<br />"; 
+                                echo "Dimensione: ".$_FILES['imagefile']['size']."<br />"; 
+                            echo "Dimensione: ".$_FILES['imagefile']['size']."<br />"; 
+                                echo "Dimensione: ".$_FILES['imagefile']['size']."<br />"; 
+                                echo "Tipo: ".$_FILES['imagefile']['type']."<br />"; 
+                            echo "Tipo: ".$_FILES['imagefile']['type']."<br />"; 
+                                echo "Tipo: ".$_FILES['imagefile']['type']."<br />"; 
+                                echo "Copia eseguita con successo.";
+                            echo "Copia eseguita con successo."; 
+                                echo "Copia eseguita con successo.";
+                            echo "<div>"
                         }
                     } 
                 ?>
             </form>
         </div>
         <footer>
-            <div class="container">
-                <p style="text-align: center">Copyright© 2019 - Nome società che non so.</p>
+            <div id="container">
+                <p style="text-align: center">Copyright© 2019 - Bast'azzardo</p>
             </div>
         </footer>
     </body>
