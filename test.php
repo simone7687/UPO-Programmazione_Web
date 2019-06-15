4355081ffa4512944e7823ec070b7002132bf040
Alessandro Spisso <sps_97@hotmail.it>
2019-06-15 16:45:44 +0200

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    @include 'config.php';
-
-    $sql = "SELECT * FROM punti_incontro p JOIN comuni c ON p.id_comune = c.id";
-    $result = mysql_query($sql);
-
-    $lol = mysql_fetch_array($result);
-    echo $lol[nome];
-?>
